5c86a682b5ee21ee499760479cf2f24651991b3b
Tholfikar <fukar.work@gmail.com>
2024-10-19 05:36:47 +0300

    complete #6 challange

---
+++ #6 Number of Days in Month (short logic)/#6 Number of Days in Month (short logic)/#6 Number of Days in Month (short logic).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool CheckLeapYear(int year)
+{
+    return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+}
+
+short NumberOfDaysInMonth(short year, short month)
+{
+    if (month < 1 || month > 12)
+        return 0;
+
+    short NumberOfDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+    return (month == 2) ? (CheckLeapYear(year) ? 29 : 28) : NumberOfDays[month - 1];
+
+}
+
+short ReadYear()
+{
+    short year;
+    cout << "Please, Enter a Year to check: ";
+    cin >> year;
+    return year;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "Please, Enter a Month to check: ";
+    cin >> Month;
+    return Month;
+}
+
+int main()
+{
+    short year = ReadYear();
+    short month = ReadMonth();
+
+    cout << "Number of Days in month [" << month << "] is: " << NumberOfDaysInMonth(year, month) << endl;
+
+}
